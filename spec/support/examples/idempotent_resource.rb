1b7b8b35db285a6813727b8a903fa21c29b17964
James Pogran <james.pogran@puppetlabs.com>
2016-12-15 09:26:27 -0500

    (MODULES-4219) Add Create/Destroy to IIS App Pool
    This adds the create/update/destroy methods for the iis_application_pool
    which handle the name, state, managedpipelinemode, and
    managedruntimeversion properties.
    
    This also adds acceptance tests covering iis_application_pool
    
    Included tests:
     - Creation of app pool resource with default params
     - Creation of app pool resource with specified params
     - Creation of app pool resource with invalid params
     - Removal of app pool resource
     - Starting a stopped application pool

--- spec/support/examples/idempotent_resource.rb
+++ spec/support/examples/idempotent_resource.rb
@@ -1,10 +1,9 @@
 shared_examples 'an idempotent resource' do
   it 'should run without errors' do
-    expect(@result.exit_code).to eq 2
+    apply_manifest(@manifest, :expect_changes => true)
   end
 
   it 'should run a second time without changes' do
-    second_result = apply_manifest(@manifest, acceptable_exit_codes: (0...256))
-    expect(second_result.exit_code).to eq 0
+    apply_manifest(@manifest, :expect_changes => false)
   end
 end
