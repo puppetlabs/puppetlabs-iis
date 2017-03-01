10a7c28b9300f69d3484503304a140c068d29114
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-03-01 15:35:16 -0800

    (MODULES-4463) Fix typo in iis_site helpers
    Previously there was a typo which caused the iis_site helper scripts to fail
    acceptance tests on Server 2008.  This commit removes the extra curly brace.

--- spec/support/utilities/iis_site.rb
+++ spec/support/utilities/iis_site.rb
@@ -2,9 +2,9 @@ def create_site(name, started, path = 'C:\inetpub\wwwroot')
   create_path(path)
   on(default, format_powershell_iis_command("New-Website -Name '#{name}' -PhysicalPath '#{path}'"))
   if(started == true)
-    command = format_powershell_iis_command("Start-Website -Name '#{name}'}")
+    command = format_powershell_iis_command("Start-Website -Name '#{name}'")
   else
-    command = format_powershell_iis_command("Stop-Website -Name '#{name}'}")
+    command = format_powershell_iis_command("Stop-Website -Name '#{name}'")
   end
   on(default, command)
 end
