d50072bda1bfc60d6b4cbcbca2cc15a1ffb6622b
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-03-10 14:30:47 -0800

    (maint) Do not print Windows 2008 powershell support to debug
    Previously, when a PowerShell command was sent to debug output it contained all
    of the PowerShell code to support the ConvertTo-JSON command however this meant
    the actual command that was issued was lost in a sea of messages.  This commit
    emits the debug message before all of the PowerShell support code is added.

--- lib/puppet/provider/iis_powershell.rb
+++ lib/puppet/provider/iis_powershell.rb
@@ -35,6 +35,8 @@ class Puppet::Provider::IIS_PowerShell < Puppet::Provider # rubocop:disable all
   end
 
   def self.run(command, check = false)
+    Puppet.debug("COMMAND: #{command}")
+
     if self.ps_major_version == 2
       # - PowerShell 2.0 does not support autoload of modules therefore we must explicitly add the WebAdministration module
       # - Must change the current location to the be the IIS: provider
@@ -46,7 +48,6 @@ class Puppet::Provider::IIS_PowerShell < Puppet::Provider # rubocop:disable all
                 command
     end
 
-    Puppet.debug("COMMAND: #{command}")
     result = ps_manager.execute(command, nil, nil)
 
     stdout      = result[:stdout]
@@ -130,7 +131,7 @@ class Puppet::Provider::IIS_PowerShell < Puppet::Provider # rubocop:disable all
     # into a PowerShell 3.0+ format.
     if result.is_a?(Hash) && result.keys[0] == 'Objects'
       return nil if result['Objects'].nil?
-      
+
       # If only a single object is returned then the result is Hash with a single 'Object' key
       # if multiple objects are returned then the result is an array of Hashes
       if result['Objects'].is_a?(Hash) && result['Objects'].keys[0] == 'Object'
