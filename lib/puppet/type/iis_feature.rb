15fbf949ef76c6109d13e3219d35b29c4a09adda
tkishel <tkishel@gmail.com>
2017-08-01 17:53:50 -0700

    (MODULES-5264) IIS: handle UNC paths
    Implement path type in iis_feature.
    Fix desc typo.

--- lib/puppet/type/iis_feature.rb
+++ lib/puppet/type/iis_feature.rb
@@ -1,5 +1,5 @@
 require 'puppet/parameter/boolean'
-require_relative '../../puppet_x/puppetlabs/iis/property/string'
+require_relative '../../puppet_x/puppetlabs/iis/property/path'
 
 Puppet::Type.newtype(:iis_feature) do
   @doc = "Manage an IIS installed features."
@@ -18,14 +18,14 @@ Puppet::Type.newtype(:iis_feature) do
   end
 
   newparam(:restart, :boolean => true) do
-    desc "Indicates whether to allow a restart if the IIS feature installationrequests one"
+    desc "Indicates whether to allow a restart if the IIS feature installation requests one"
   end
 
   newparam(:include_management_tools, :boolean => true) do
     desc "Indicates whether to automatically install all managment tools for a given IIS feature"
   end
 
-  newparam(:source, :parent => PuppetX::PuppetLabs::IIS::Property::String) do
+  newparam(:source, :parent => PuppetX::PuppetLabs::IIS::Property::Path) do
     desc "Optionally include a source path for the installation media for an IIS feature"
   end
 
