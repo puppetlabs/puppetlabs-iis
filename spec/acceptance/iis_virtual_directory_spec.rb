4ad710b02acc444a75a426e80a04d1faa2a8bc76
James Pogran <jpogran@outlook.com>
2017-10-23 15:44:44 -0400

    (maint) Fix iis virt dir tests

--- spec/acceptance/iis_virtual_directory_spec.rb
+++ spec/acceptance/iis_virtual_directory_spec.rb
@@ -43,6 +43,37 @@ describe 'iis_virtual_directory' do
         remove_vdir(@virt_dir_name)
       end
     end
+    
+    context 'name allows slashes' do
+      context 'simple case' do
+        before(:all) do
+          @virt_dir_name = "#{SecureRandom.hex(10)}"
+          create_path('c:\inetpub\test_site')
+          create_path('c:\inetpub\test_vdir')
+          create_path('c:\inetpub\deeper')
+          create_site(@site_name, true)
+          @manifest  = <<-HERE
+          iis_virtual_directory{ "test_vdir":
+            ensure       => 'present',
+            sitename     => "#{@site_name}",
+            physicalpath => 'c:\\inetpub\\test_vdir',
+          }->
+          iis_virtual_directory { 'test_vdir\deeper':
+            name         => 'test_vdir\deeper',
+            ensure	     => 'present',
+            sitename     => '#{@site_name}',
+            physicalpath => 'c:\\inetpub\\deeper',
+          }
+          HERE
+        end
+        
+        it_behaves_like 'an idempotent resource'
+
+        after(:all) do
+          remove_vdir(@virt_dir_name)
+        end
+      end
+    end
 
     context 'with invalid' do
       context 'physicalpath parameter defined' do
@@ -92,48 +123,6 @@ describe 'iis_virtual_directory' do
 
           puppet_resource_should_show('ensure', 'absent')
         end
-
-        after(:all) do
-          remove_vdir(@virt_dir_name)
-        end
-      end
-
-      context 'name allows slashes' do
-        before(:all) do
-          @virt_dir_name = "#{SecureRandom.hex(10)}"
-          @manifest  = <<-HERE
-          file { 'c:\\inetpub\\test_site':
-            ensure          => 'present',
-          }->
-          iis_site { "test_site":
-            name            => "test_site",
-            ensure          => 'present',
-            physicalpath    => 'c:\\inetpub\\test_site',
-            applicationpool => 'DefaultApplicationPool',
-          }
-
-          file { 'c:\\inetpub\\test_vdir':
-            ensure       => 'present',
-          }->
-          iis_virtual_directory{ "test_vdir":
-            ensure       => 'present',
-            sitename     => "test_site",
-            physicalpath => 'c:\\inetpub\\test_vdir',
-          }
-
-          file { 'c:\\inetpub\\deeper':
-            ensure       => 'present',
-          }->
-          iis_virtual_directory { 'test_vdir\deeper':
-            name         => 'test_vdir\deeper',
-            ensure	     => 'present',
-            sitename     => 'test_site',
-            physicalpath => 'c:\\inetpub\\deeper',
-          }
-          HERE
-        end
-
-        it_behaves_like 'an idempotent resource'
 
         after(:all) do
           remove_vdir(@virt_dir_name)
