f50eda76eefcec6257a8812571789bdae2266b67
Nick Hilhorst <nick.hilhorst@asr.nl>
2017-10-25 18:25:49 +0200

    Applied requested changes

--- lib/puppet/provider/iis_webconfigurationproperty/iis_webconfigurationproperty.rb
+++ lib/puppet/provider/iis_webconfigurationproperty/iis_webconfigurationproperty.rb
@@ -1,52 +1,48 @@
-Puppet::Type.type(:iis_webconfigurationproperty).provide(:iis_webconfigurationproperty) do
-	confine :operatingsystem => :windows
-	defaultfor :operatingsystem => :windows
+require File.join(File.dirname(__FILE__), '../../../puppet/provider/iis_powershell')
 
-	commands :powershell =>
-		if File.exists?("#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe")
-			"#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe"
-		elsif File.exists?("#{ENV['SYSTEMROOT']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe")
-			"#{ENV['SYSTEMROOT']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe"
-		else
-			'powershell.exe'
-		end
+Puppet::Type.type(:iis_webconfigurationproperty).provide(:iis_webconfigurationproperty, parent: Puppet::Provider::IIS_PowerShell) do
+  confine    :iis_version     => ['7.0', '7.5', '8.0', '8.5', '10.0']
+  confine    :operatingsystem => :windows
+  defaultfor :operatingsystem => :windows
 
-	desc <<-EOT
-		Manage IIS web configuration properties.
+  desc <<-EOT
+    Manage IIS web configuration properties.
 
-		Example:
+    Example:
 
-		iis_webconfigurationproperty {'Config History on D drive':
-			filter => '/system.applicationHost/configHistory',
-			name   => 'path',
-			value  => 'D:\inetpub\History',
-		}
-	EOT
+    iis_webconfigurationproperty {'Config History on D drive':
+      filter => '/system.applicationHost/configHistory',
+      name   => 'path',
+      value  => 'D:\inetpub\History',
+    }
+  EOT
 
-	def address(resource)
-		array_address = []
-		resource.each do |key,value|
-			case key
-			when :pspath
-				array_address << "-PSPath \"#{value}\"" unless value == 'MACHINE/WEBROOT/APPHOST'
-			when :location
-				array_address << "-Location \"#{value}\"" unless value == ''
-			when :filter
-				array_address << "-Filter \"#{value}\""
-			when :name
-				array_address << "-Name \"#{value}\""
-			end
-		end
-		array_address
-	end
+  def address(resource)
+    array_address = []
+    resource.each do |key,value|
+      case key
+      when :pspath
+        array_address << "-PSPath \"#{value}\"" unless value == 'MACHINE/WEBROOT/APPHOST'
+      when :location
+        array_address << "-Location \"#{value}\"" unless value == ''
+      when :filter
+        array_address << "-Filter \"#{value}\""
+      when :name
+        array_address << "-Name \"#{value}\""
+      end
+    end
+    array_address
+  end
 
-	def value
-		str_address = address(resource.to_hash).join(" ")
-		powershell("(Get-WebConfigurationProperty #{str_address}).Value").strip
-	end
+  def value
+    str_address = address(resource.to_hash).join(" ")
+    cmd = "(Get-WebConfigurationProperty #{str_address}).Value"
+    self.class.run(cmd).strip
+  end
 
-	def value=(value)
-		str_address = address(resource.to_hash).join(" ")
-		powershell("Set-WebConfigurationProperty #{str_address} -Value \"#{value}\"")
-	end
+  def value=(value)
+    str_address = address(resource.to_hash).join(" ")
+    cmd = "Set-WebConfigurationProperty #{str_address} -Value \"#{value}\""
+    self.class.run(cmd)
+  end
 end
--- lib/puppet/provider/iis_webhandler/iis_webhandler.rb
+++ lib/puppet/provider/iis_webhandler/iis_webhandler.rb
@@ -1,197 +1,186 @@
-require 'json'
+require File.join(File.dirname(__FILE__), '../../../puppet/provider/iis_powershell')
 
-Puppet::Type.type(:iis_webhandler).provide(:iis_webhandler) do
-	confine :operatingsystem => :windows
-	defaultfor :operatingsystem => :windows
+Puppet::Type.type(:iis_webhandler).provide(:iis_webhandler, parent: Puppet::Provider::IIS_PowerShell) do
+  confine    :iis_version     => ['7.0', '7.5', '8.0', '8.5', '10.0']
+  confine    :operatingsystem => :windows
+  defaultfor :operatingsystem => :windows
 
-	commands :powershell =>
-		if File.exists?("#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe")
-			"#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe"
-		elsif File.exists?("#{ENV['SYSTEMROOT']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe")
-			"#{ENV['SYSTEMROOT']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe"
-		else
-			'powershell.exe'
-		end
+  desc <<-EOT
+    Manage IIS request handlers.
 
-	desc <<-EOT
-		Manage IIS request handlers.
+    Example:
+    iis_webhandler {'svc-ISAPI-4.0_32bit':
+      path            => '*.svc',
+      verb            => '*',
+      modules         => 'IsapiModule',
+      scriptprocessor => '%windir%\\Microsoft.NET\\Framework\\v4.0.30319\\aspnet_isapi.dll',
+    }
+  EOT
 
-		Example:
-		iis_webhandler {'svc-ISAPI-4.0_32bit':
-			path            => '*.svc',
-			verb            => '*',
-			modules         => 'IsapiModule',
-			scriptprocessor => '%windir%\\Microsoft.NET\\Framework\\v4.0.30319\\aspnet_isapi.dll',
-		}
-	EOT
+  mk_resource_methods
 
-	mk_resource_methods
+  def initialize(value={})
+    super(value)
+    @property_flush = {}
+    @convert = {
+      :path            => 'Path',
+      :verb            => 'Verb',
+      :type            => 'Type',
+      :modules         => 'Modules',
+      :precondition    => 'Precondition',
+      :scriptprocessor => 'ScriptProcessor',
+      :resourcetype    => 'ResourceType',
+      :requireaccess   => 'RequiredAccess',
+    }
+  end
 
-	def initialize(value={})
-		super(value)
-		@property_flush = {}
-		@convert = {
-			:path            => 'Path',
-			:verb            => 'Verb',
-			:type            => 'Type',
-			:modules         => 'Modules',
-			:precondition    => 'Precondition',
-			:scriptprocessor => 'ScriptProcessor',
-			:resourcetype    => 'ResourceType',
-			:requireaccess   => 'RequiredAccess',
-		}
-	end
+  def self.get_webhandlers
+    cmd = "Get-WebConfiguration -Recurse -Filter /system.webServer/handlers/add | select Name,Path,Verb,Type,Modules,ScriptProcessor,Precondition,ResourceType,RequireAccess,Location,PSPath"
+    result = self.class.run(cmd)
+    # convert all keys to lowercase symbols and all values to string
+    result.to_a.map {|hash| hash.inject({}){|memo,(k,v)| memo[k.downcase.to_sym] = v.to_s; memo}}
+  end
 
-	def exists?
-		@property_hash[:ensure] == :present
-	end
+  def self.instances
+    get_webhandlers.map do |webhandler|
+      webhandler[:ensure] = :present
+      new(webhandler)
+    end
+  end
 
-	def self.get_webhandlers
-		output = powershell("Get-WebConfiguration -Recurse -Filter /system.webServer/handlers/add | select Name,Path,Verb,Type,Modules,ScriptProcessor,Precondition,ResourceType,RequireAccess,Location,PSPath | ConvertTo-Json")
-		json = JSON.parse(output)
-		json.map {|hash| hash.inject({}){|memo,(k,v)| memo[k.downcase.to_sym] = v; memo}}
-	end
+  def self.prefetch(resources)
+    # https://tickets.puppetlabs.com/browse/PUP-5302
+    catalog = resources.values.first.catalog
 
-	def self.instances
-		get_webhandlers.map do |webhandler|
-			webhandler[:ensure] = :present
-			new(webhandler)
-		end
-	end
+    webhandlers = catalog.resources.find_all do |e|
+      e.class.to_s.downcase == 'puppet::type::iis_webhandler'
+    end
 
-	def self.prefetch(resources)
-		# https://tickets.puppetlabs.com/browse/PUP-5302
-		catalog = resources.values.first.catalog
+    webhandlers_keys = Hash[webhandlers.map(&:uniqueness_key).zip(webhandlers)]
 
-		webhandlers = catalog.resources.find_all do |e|
-			e.class.to_s.downcase == 'puppet::type::iis_webhandler'
-		end
+    webhandler_providers = instances
 
-		webhandlers_keys = Hash[webhandlers.map(&:uniqueness_key).zip(webhandlers)]
+    webhandlers_keys.keys.each do |resource_location, resource_name, resource_pspath|
+      # Accept empty location and pspath and interpret them as default
+      if resource_location.nil?
+        resource_location = ''
+      end
+      if resource_pspath.nil?
+        resource_pspath = 'MACHINE/WEBROOT/APPHOST'
+      end
+      provider = webhandler_providers.find do |webhandler|
+        webhandler_name = webhandler.name
+        webhandler_location = webhandler.location
+        webhandler_pspath = webhandler.pspath
+        ((resource_name == webhandler_name)&&(resource_location == webhandler_location)&&(resource_pspath == webhandler_pspath))
+      end
+      if provider
+        provider_name = provider.name
+        provider_location = provider.location
+        provider_pspath = provider.pspath
 
-		webhandler_providers = instances
+        resource = webhandlers_keys[[provider_location, provider_name, provider_pspath]]
 
-		webhandlers_keys.keys.each do |resource_location, resource_name, resource_pspath|
-			# Accept empty location and pspath and interpret them as default
-			if resource_location.nil?
-				resource_location = ''
-			end
-			if resource_pspath.nil?
-				resource_pspath = 'MACHINE/WEBROOT/APPHOST'
-			end
-			provider = webhandler_providers.find do |webhandler|
-				webhandler_name = webhandler.name
-				webhandler_location = webhandler.location
-				webhandler_pspath = webhandler.pspath
-				((resource_name == webhandler_name)&&(resource_location == webhandler_location)&&(resource_pspath == webhandler_pspath))
-			end
-			if provider
-				provider_name = provider.name
-				provider_location = provider.location
-				provider_pspath = provider.pspath
+        resource.provider = provider
+      end
+    end
+  end
 
-				resource = webhandlers_keys[[provider_location, provider_name, provider_pspath]]
+  def path=(value)
+    @property_flush[:path] = value
+  end
 
-				resource.provider = provider
-			end
-		end
-	end
+  def verb=(value)
+    @property_flush[:verb] = value
+  end
 
-	def path=(value)
-		@property_flush[:path] = value
-	end
+  def type=(value)
+    @property_flush[:type] = value
+  end
 
-	def verb=(value)
-		@property_flush[:verb] = value
-	end
+  def modules=(value)
+    @property_flush[:modules] = value
+  end
 
-	def type=(value)
-		@property_flush[:type] = value
-	end
+  def scriptprocessor=(value)
+    @property_flush[:scriptprocessor] = value
+  end
 
-	def modules=(value)
-		@property_flush[:modules] = value
-	end
+  def precondition=(value)
+    @property_flush[:precondition] = value
+  end
 
-	def scriptprocessor=(value)
-		@property_flush[:scriptprocessor] = value
-	end
+  def resourcetype=(value)
+    @property_flush[:resourcetype] = value
+  end
 
-	def precondition=(value)
-		@property_flush[:precondition] = value
-	end
+  def requireaccess=(value)
+    @property_flush[:requireaccess] = value
+  end
 
-	def resourcetype=(value)
-		@property_flush[:resourcetype] = value
-	end
+  def arguments(resource)
+    array_arguments = []
+    labels = [:path, :verb, :type, :modules, :scriptprocessor, :precondition, :resourcetype, :requireaccess]
+    resource.select{ |key, value| labels.include?(key)}.each do |key,value|
+      case key
+      when :requireaccess
+        array_arguments << '-RequiredAccess'
+        array_arguments << value.to_s.capitalize
+      else
+        array_arguments << "-#{@convert[key]}"
+        array_arguments << %Q{"#{value}"}
+      end
+    end
+    array_arguments
+  end
 
-	def requireaccess=(value)
-		@property_flush[:requireaccess] = value
-	end
+  def address(resource)
+    array_address = []
+    resource.each do |key,value|
+      case key
+      when :pspath
+        array_address << "-PSPath \"#{value}\"" unless value == 'MACHINE/WEBROOT/APPHOST'
+      when :location
+        array_address << "-Location \"#{value}\"" unless value == ''
+      end
+    end
+    array_address
+  end
 
-	def arguments(resource)
-		array_arguments = []
-		labels = [:path, :verb, :type, :modules, :scriptprocessor, :precondition, :resourcetype, :requireaccess]
-		resource.select{ |key, value| labels.include?(key)}.each do |key,value|
-			case key
-			when :requireaccess
-				array_arguments << '-RequiredAccess'
-				array_arguments << value.to_s.capitalize
-			else
-				array_arguments << "-#{@convert[key]}"
-				array_arguments << %Q{"#{value}"}
-			end
-		end
-		array_arguments
-	end
+  def set_webhandler
+    if @property_flush
+      str_address = address(resource.to_hash).join(" ")
+      case @property_flush[:ensure]
+      when :present
+        # create
+        if not( resource[:path] and resource[:verb] )
+          raise ArgumentError, "path and verb are mandatory for iis_webhandler"
+        end
+        str_arguments = arguments(resource.to_hash).join(" ")
+        powershell("New-WebHandler -Name #{resource[:name]} #{str_address} #{str_arguments}")
+      when :absent
+        # destroy
+        powershell("Remove-WebHandler -Name #{resource[:name]} #{str_address}")
+      when nil
+        # update
+        str_arguments = arguments(@property_flush).join(" ")
+        if ! array_arguments.empty?
+          powershell("Set-WebHandler -Name #{resource[:name]} #{str_address} #{str_arguments}")
+        end
+      end
+    end
+  end
 
-	def address(resource)
-		array_address = []
-		resource.each do |key,value|
-			case key
-			when :pspath
-				array_address << "-PSPath \"#{value}\"" unless value == 'MACHINE/WEBROOT/APPHOST'
-			when :location
-				array_address << "-Location \"#{value}\"" unless value == ''
-			end
-		end
-		array_address
-	end
+  def flush
+    set_webhandler
+    @property_hash = self.class.get_webhandlers()
+  end
 
-	def set_webhandler
-		if @property_flush
-			str_address = address(resource.to_hash).join(" ")
-			case @property_flush[:ensure]
-			when :present
-				# create
-				if not( resource[:path] and resource[:verb] )
-					raise ArgumentError, "path and verb are mandatory for iis_webhandler"
-				end
-				str_arguments = arguments(resource.to_hash).join(" ")
-				powershell("New-WebHandler -Name #{resource[:name]} #{str_address} #{str_arguments}")
-			when :absent
-				# destroy
-				powershell("Remove-WebHandler -Name #{resource[:name]} #{str_address}")
-			when nil
-				# update
-				str_arguments = arguments(@property_flush).join(" ")
-				if ! array_arguments.empty?
-					powershell("Set-WebHandler -Name #{resource[:name]} #{str_address} #{str_arguments}")
-				end
-			end
-		end
-	end
+  def create
+    @property_flush[:ensure] = :present
+  end
 
-	def flush
-		set_webhandler
-		@property_hash = self.class.get_webhandlers()
-	end
-
-	def create
-		@property_flush[:ensure] = :present
-	end
-
-	def destroy
-		@property_flush[:ensure] = :absent
-	end
+  def destroy
+    @property_flush[:ensure] = :absent
+  end
 end
