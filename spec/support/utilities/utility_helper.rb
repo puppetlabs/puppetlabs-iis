1ed234e7fa356a8593648648e051ad4499fc429f
jordanbreen28 <jordan.breen@perforce.com>
2023-04-11 11:28:54 +0100

    (CONT-782) - Correct Lint/UselessAssignment

--- spec/support/utilities/utility_helper.rb
+++ spec/support/utilities/utility_helper.rb
@@ -22,7 +22,7 @@ def create_selfsigned_cert(dnsname)
 
   # Defaults to personal machine store
   command = format_powershell_iis_command("& CERTUTIL -f -p puppet -importpfx '#{cert_filename_dest_windows}' NoRoot ")
-  result = run_shell(command)
+  run_shell(command)
 
   # These commands are executed in bash therefore things need to be escaped properly
   command = format_powershell_iis_command("(Get-ChildItem -Path 'Cert:\\LocalMachine\\My' | Where-Object { \$_.Subject -eq 'CN=#{dnsname}'} | Select-Object -First 1).Thumbprint")
